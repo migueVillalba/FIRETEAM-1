53f8c643afaf3a5a69768271d2b3fb35e4540f6a
black <black@CC-HAVOC>
2021-01-25 10:39:03 -0600

    Upadte LFGM

---
+++ src/fireteamfinal/FireteamFinal.java
@@ -0,0 +1,16 @@
+package fireteamfinal;
+
+/**
+ *
+ * @author black
+ */
+public class FireteamFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
